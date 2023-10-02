befbbf63ed8fe485c80a390ce30ab209f2bb769f
Joe McNally <joe.t.mcnally@hotmail.com>
2023-10-02 01:29:34 +0100

    Correct deprecated csrf disable

--- src/main/java/pacmanapi/config/Security.java
+++ src/main/java/pacmanapi/config/Security.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,8 +18,8 @@ public class Security {
                     .anyRequest().authenticated()
             )
             .httpBasic(withDefaults())
-            .formLogin(withDefaults());
-    http.csrf().disable();
+            .formLogin(withDefaults())
+            .csrf(AbstractHttpConfigurer::disable);
     return http.build();
   }
 }
