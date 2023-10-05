159b4f3390853506cdaf2a4f4e71bba734a3f247
Joe McNally <joe.t.mcnally@hotmail.com>
2023-10-05 21:07:18 +0100

    Committed the wrong test file

---
+++ src/test/java/pacmanapi/unit/utility/AuthenticatorTest.java
@@ -0,0 +1,41 @@
+package pacmanapi.unit.utility;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.AfterEach;
+import org.mockito.MockedStatic;
+import pacmanapi.model.User;
+import pacmanapi.utility.Authenticator;
+
+import javax.crypto.SecretKey;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class AuthenticatorTest {
+  private final SecretKey secretKey = mock(SecretKey.class);
+  private final MockedStatic<Jwts> jwtsMockedStatic = mockStatic(Jwts.class);
+  private final User user = mock(User.class);
+  private final JwtBuilder builder = mock(JwtBuilder.class);
+
+  @AfterEach
+  public void afterEach() {
+    reset(secretKey, user, builder);
+    jwtsMockedStatic.close();
+  }
+
+  public void generateJwtCreatesAJwt() {
+    jwtsMockedStatic.when(Jwts::builder).thenReturn(builder);
+    when(user.getUsername()).thenReturn("Pingu");
+    when(builder.claim("username", "Pingu")).thenReturn(builder);
+    when(builder.signWith(secretKey)).thenReturn(builder);
+    when(builder.compact()).thenReturn("ANotSoSecretKey");
+
+    Authenticator authenticator = new Authenticator(secretKey);
+    assertEquals("ANotSoSecretKey", authenticator.generateJwt(user));
+    jwtsMockedStatic.verify(Jwts::builder);
+    verify(builder.claim("username", "Pingu"));
+    verify(builder.signWith(secretKey));
+    verify(builder.compact());
+  }
+}
