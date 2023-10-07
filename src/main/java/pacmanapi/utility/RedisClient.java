8693a02d141bec1c3796e010072836ee8457f5bf
Joe McNally <joe.t.mcnally@hotmail.com>
2023-10-07 17:39:24 +0100

    Add individual score management
    Make two new functions in the redis client that an retrieve and add a single entry to the scores sorted set.

--- src/main/java/pacmanapi/utility/RedisClient.java
+++ src/main/java/pacmanapi/utility/RedisClient.java
@@ -8,21 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class RedisClient {
+  private final String key = "scores";
   private final JedisPooled jedis;
 
   public RedisClient(JedisPooled jedis) {
     this.jedis = jedis;
   }
 
+  public int getScore(String username) {
+    return this.jedis.zscore(this.key, username).intValue();
+  }
+
+  public void saveScore(String username, int points) {
+    this.jedis.zadd(this.key, points, username);
+  }
+
   public ArrayList<Score> getTopTenScores() {
-    List<Tuple> scoresData = this.jedis.zrevrangeWithScores("scores", 0, 10);
+    List<Tuple> scoresData = this.jedis.zrevrangeWithScores(this.key, 0, 10);
     return this.convertScoresData(scoresData);
   }
 
   private ArrayList<Score> convertScoresData(List<Tuple> scoresData) {
     ArrayList<Score> scores = new ArrayList<>();
     for (Tuple score : scoresData) {
-      scores.add(new Score(score.getElement(), ((int) score.getScore())));
+      scores.add(new Score(score.getElement(), (int) score.getScore()));
     }
     return scores;
   }
